9301949e54d2546a257bc863d993359c5fcc518c
Filip Chalupa <chalupa.filip@gmail.com>
2021-04-22 22:54:06 +0200

    Task 5

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ for (let i = 0; i < fields.length; i++) {
 		field.classList.add(`board__field--${currentPlayer}`)
 		field.disabled = true
 
+		if (isWinningMove(field)) {
+			let message = 'Vyhrál křížek!'
+			if (currentPlayer === 'circle') {
+				message = 'Vyhrálo kolečko!'
+			}
+
+			setTimeout(() => {
+				if (confirm(`${message} Spustit novou hru?`)) {
+					location.reload()
+				}
+			}, 150)
+		}
+
 		currentSymbol.classList.remove(`current-player__symbol--${currentPlayer}`)
 		if (currentPlayer === 'circle') {
 			currentPlayer = 'cross'
@@ -22,4 +35,83 @@ for (let i = 0; i < fields.length; i++) {
 	setTimeout(() => {
 		field.style.animationPlayState = 'running'
 	}, i * 8 + (i % boardSize) * 32)
+}
+
+const getPosition = (field) => {
+	let fieldIndex = 0
+	while (fieldIndex < fields.length) {
+		if (field === fields[fieldIndex]) {
+			break
+		}
+		fieldIndex++
+	}
+
+	return {
+		row: Math.floor(fieldIndex / boardSize),
+		column: fieldIndex % boardSize,
+	}
+}
+
+const getField = (row, column) => fields[row * boardSize + column]
+
+const getSymbol = (field) => {
+	if (field.classList.contains('board__field--cross')) {
+		return 'cross'
+	} else if (field.classList.contains('board__field--circle')) {
+		return 'circle'
+	}
+}
+
+const symbolsToWin = 5
+const isWinningMove = (field) => {
+	const origin = getPosition(field)
+	const symbol = getSymbol(field)
+
+	let i
+
+	let inRow = 1 // Jednička pro právě vybrané políčko
+	// Koukni doleva
+	i = origin.column
+	while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+		inRow++
+		i--
+	}
+
+	// Koukni doprava
+	i = origin.column
+	while (
+		i < boardSize - 1 &&
+		symbol === getSymbol(getField(origin.row, i + 1))
+	) {
+		inRow++
+		i++
+	}
+
+	if (inRow >= symbolsToWin) {
+		return true
+	}
+
+	let inColumn = 1
+	// Koukni nahoru
+	i = origin.row
+	while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+		inColumn++
+		i--
+	}
+
+	// Koukni dolu
+	i = origin.row
+	while (
+		i < boardSize - 1 &&
+		symbol === getSymbol(getField(i + 1, origin.column))
+	) {
+		inColumn++
+		i++
+	}
+
+	if (inColumn >= symbolsToWin) {
+		return true
+	}
+
+	return false
 }
