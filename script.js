8ec89fb1c947124976cf33d9c2fb673fdf764a94
Filip Chalupa <chalupa.filip@gmail.com>
2023-02-05 19:00:20 +0100

    Use library

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { findWinner } from 'https://unpkg.com/piskvorky@0.0.4/dist/index.es.js'
 let currentPlayer = 'circle'
 const fields = document.querySelectorAll('.board__field')
 const currentSymbol = document.querySelector('.current-player__symbol')
@@ -9,10 +10,25 @@ for (let i = 0; i < fields.length; i++) {
 		field.classList.add(`board__field--${currentPlayer}`)
 		field.disabled = true
 
-		if (isWinningMove(field)) {
-			let message = 'Vyhrál křížek!'
-			if (currentPlayer === 'circle') {
+		const board = []
+		fields.forEach((field) => {
+			if (field.classList.contains('board__field--circle')) {
+				board.push(1)
+			} else if (field.classList.contains('board__field--cross')) {
+				board.push(2)
+			} else {
+				board.push(0)
+			}
+		})
+
+		const winner = findWinner(board)
+
+		if (winner !== 0) {
+			let message = 'Remíza!'
+			if (winner === 1) {
 				message = 'Vyhrálo kolečko!'
+			} else if (winner === 2) {
+				message = 'Vyhrál křížek!'
 			}
 
 			setTimeout(() => {
@@ -35,80 +51,4 @@ for (let i = 0; i < fields.length; i++) {
 	setTimeout(() => {
 		field.style.animationPlayState = 'running'
 	}, i * 8 + (i % boardSize) * 32)
-}
-
-const getPosition = (field) => {
-	let fieldIndex = 0
-	while (fieldIndex < fields.length && field !== fields[fieldIndex]) {
-		fieldIndex++
-	}
-
-	return {
-		row: Math.floor(fieldIndex / boardSize),
-		column: fieldIndex % boardSize,
-	}
-}
-
-const getField = (row, column) => fields[row * boardSize + column]
-
-const getSymbol = (field) => {
-	if (field.classList.contains('board__field--cross')) {
-		return 'cross'
-	} else if (field.classList.contains('board__field--circle')) {
-		return 'circle'
-	}
-}
-
-const symbolsToWin = 5
-const isWinningMove = (field) => {
-	const origin = getPosition(field)
-	const symbol = getSymbol(field)
-
-	let i
-
-	let inRow = 1 // Jednička pro právě vybrané políčko
-	// Koukni doleva
-	i = origin.column
-	while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
-		inRow++
-		i--
-	}
-
-	// Koukni doprava
-	i = origin.column
-	while (
-		i < boardSize - 1 &&
-		symbol === getSymbol(getField(origin.row, i + 1))
-	) {
-		inRow++
-		i++
-	}
-
-	if (inRow >= symbolsToWin) {
-		return true
-	}
-
-	let inColumn = 1
-	// Koukni nahoru
-	i = origin.row
-	while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
-		inColumn++
-		i--
-	}
-
-	// Koukni dolu
-	i = origin.row
-	while (
-		i < boardSize - 1 &&
-		symbol === getSymbol(getField(i + 1, origin.column))
-	) {
-		inColumn++
-		i++
-	}
-
-	if (inColumn >= symbolsToWin) {
-		return true
-	}
-
-	return false
 }
