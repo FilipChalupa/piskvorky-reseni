580da20395c9172b117ec40023af7aaf34d23070
Filip Chalupa <chalupa.filip@gmail.com>
2023-04-21 01:31:05 +0200

    AI

--- hra.js
+++ hra.js
@@ -13,7 +13,36 @@ document
 		}
 	})
 
-const handleClick = (event) => {
+const createBoard = () => {
+	return Array.from(fields).map((field) => {
+		if (field.classList.contains('board__field--circle')) {
+			return 'o'
+		}
+		if (field.classList.contains('board__field--cross')) {
+			return 'x'
+		}
+		return '_'
+	})
+}
+
+const lockBoard = () => {
+	fields.forEach((field) => {
+		field.disabled = true
+	})
+}
+
+const unlockBoard = () => {
+	fields.forEach((field) => {
+		if (
+			!field.classList.contains('board__field--circle') &&
+			!field.classList.contains('board__field--cross')
+		) {
+			field.disabled = false
+		}
+	})
+}
+
+const handleClick = async (event) => {
 	const field = event.target
 	field.classList.add(`board__field--${currentPlayer}`)
 	field.disabled = true
@@ -29,19 +58,11 @@ const handleClick = (event) => {
 
 	currentPlayerSymbol.classList.add(`current-player__symbol--${currentPlayer}`)
 
-	const board = Array.from(fields).map((field) => {
-		if (field.classList.contains('board__field--circle')) {
-			return 'o'
-		}
-		if (field.classList.contains('board__field--cross')) {
-			return 'x'
-		}
-		return '_'
-	})
+	const board = createBoard()
 
 	const winner = findWinner(board)
-	setTimeout(() => {
-		if (winner === 'o' || winner === 'x' || winner === 'tie') {
+	if (winner === 'o' || winner === 'x' || winner === 'tie') {
+		setTimeout(() => {
 			if (winner === 'o') {
 				alert('Vyhrálo kolečko!')
 			} else if (winner === 'x') {
@@ -50,8 +71,28 @@ const handleClick = (event) => {
 				alert('Hra skončila nerozhodně.')
 			}
 			location.reload()
-		}
-	}, 500)
+		}, 500)
+	} else if (currentPlayer === 'cross') {
+		lockBoard()
+		const response = await fetch(
+			'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
+			{
+				method: 'POST',
+				headers: {
+					'Content-type': 'application/json',
+				},
+				body: JSON.stringify({
+					board,
+					player: 'x',
+				}),
+			},
+		)
+		const {
+			position: { x, y },
+		} = await response.json()
+		unlockBoard()
+		fields[x + y * 10].click()
+	}
 }
 
 fields.forEach((field) => {
